db2d56e08b91f9013fa85792485afd26c19f6818
Dmitry <dima6061551@mail.ru>
2023-10-06 12:25:44 +0300

    Add project files.

---
+++ JsonParser/PathItemObject.cs
@@ -0,0 +1,16 @@
+﻿public class PathItemObject
+{
+    public object? post;
+    public string? summary;
+    public string? description;
+    public object? get;
+    public object? put;
+    public object? delete;
+    public object? options;
+    public object? head;
+    public object? patch;
+    public object? trace;
+    public object?[]? servers;
+    public object?[]? parameters;
+}
+
---
+++ JsonParser/Program.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
+
+
+string swaggerJson;
+JsonDocument document;
+var pathFrom = Path.GetFullPath("D:\\Work\\swagger\\swagger.json");
+var pathTo = Path.GetFullPath("D:\\Work\\swagger\\swaggerGet.json");
+try
+{
+	swaggerJson = File.ReadAllText(pathFrom);
+	document = JsonDocument.Parse(swaggerJson);
+}
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+	throw;
+}
+
+JsonElement paths = document.RootElement.GetProperty("paths");
+
+var myDeserializedPaths = JsonConvert.DeserializeObject<Dictionary<string,PathItemObject>>(paths.GetRawText());
+Dictionary<string, PathItemObject> necessaryPaths = new();
+foreach (var item in myDeserializedPaths)
+{
+    if (item.Value.get is not null)
+    {
+        necessaryPaths.Add(item.Key,new PathItemObject {
+            get = item.Value.get,
+            description = item.Value.description,
+            head = item.Value.head,
+            options = item.Value.options,
+            parameters = item.Value.parameters,
+            servers = item.Value.servers,
+            trace = item.Value.trace,
+            summary = item.Value.summary
+        });
+    }
+}
+var jsonStringWithnecessaryPaths = JsonConvert.SerializeObject(necessaryPaths,
+                            Newtonsoft.Json.Formatting.None,
+                            new JsonSerializerSettings
+                            {
+                                NullValueHandling = NullValueHandling.Ignore
+                            });
+
+var arr = JsonConvert.DeserializeObject(swaggerJson);
+
+if ((arr as JObject).ContainsKey("paths"))
+{
+    ((JObject)arr)["paths"] = JObject.Parse(jsonStringWithnecessaryPaths);
+};
+
+var end = JsonConvert.SerializeObject(arr);
+
+if (File.Exists(pathTo))
+{
+    File.WriteAllText(pathTo, end);
+}
+else
+{
+    File.Create(pathTo);
+    File.WriteAllText(pathTo, end);
+}
+
